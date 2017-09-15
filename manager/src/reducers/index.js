dc78e51eaaecd8c4a1cfafa0a60bec3d7abe4c3a
Valleyaapo <aapohlaakso@gmail.com>
2017-09-15 15:46:14 +0300

    new updates

--- manager/src/reducers/index.js
+++ manager/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import EmployeeFormRedcuer from './EmployeeFormReducer';
+import EmployeeRedcuer from './EmployeeReducer';
 
 export default combineReducers({
     auth: AuthReducer,
-    employeeForm: EmployeeFormRedcuer
+    employeeForm: EmployeeFormRedcuer,
+    employees: EmployeeRedcuer
 });
